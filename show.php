174972f992026602c9df3797086a0bd39faeae28
shubhangitungar <76914626+shubhangitungar@users.noreply.github.com>
2021-01-04 09:54:27 +0530

    added create button

--- show.php
+++ show.php
@@ -1,8 +1,11 @@
 <?php
 include 'database.php';
 get_records();
-
 ?>
 
-
-
+<link rel="stylesheet" type="text/css" href="register.css">
+<form name="create" method="get" action="create.php" >
+<center>
+<input type="submit" name="create" class="button1" value="Create New Record">
+</center>
+</form>
